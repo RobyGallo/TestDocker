dc3da28b4746902f4bbc051cfc05ace74b4dd1e9
Roberto Galliari <roberto.galliari@par-tec.it>
2023-12-15 18:18:07 +0100

    test1

--- src/test/java/a/ATest.java
+++ src/test/java/a/ATest.java
@@ -18,15 +18,7 @@ package a;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Locale;
-import java.util.Set;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validator;
+ 
 
 /**
  * @author Michael Isvy Simple test to make sure that Bean Validation is working (useful
@@ -34,27 +26,13 @@ import jakarta.validation.Validator;
  */
 class ATest {
 
-	private Validator createValidator() {
-		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
-		localValidatorFactoryBean.afterPropertiesSet();
-		return localValidatorFactoryBean;
-	}
+	 
 
 	@Test
 	void shouldNotValidateWhenFirstNameEmpty() {
 
-		LocaleContextHolder.setLocale(Locale.ENGLISH);
-		Person person = new Person();
-		person.setFirstName("");
-		person.setLastName("smith");
-
-		Validator validator = createValidator();
-		Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
-
-		assertThat(constraintViolations).hasSize(1);
-		ConstraintViolation<Person> violation = constraintViolations.iterator().next();
-		assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
-		assertThat(violation.getMessage()).isEqualTo("must not be blank");
+		String a = "roby";
+		assertThat(a.isEqualTo("roby");
 	}
 
 }
